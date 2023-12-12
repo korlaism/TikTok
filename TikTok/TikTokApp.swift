26981edd04e9c3b92404ce6de544326939c55962
GAURAV SINGH <gauravkorlasingh@gmail.com>
2023-12-12 18:02:12 +0530

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Gaurav Singh on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
